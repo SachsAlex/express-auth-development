70223a6ad7ff234de3e7e5867147d42f65be8880
SachsAlex <alexander.sachs@docc.techstarter.de>
2024-05-03 17:51:26 +0200

    Initial commit

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+const { AuthRouter } = require("./auth");
+const { UserRouter } = require("./users");
+const authMiddleWare = require("../middlewares/authMiddleware");
+
+const AppRouter = Router();
+
+AppRouter.use("/auth", AuthRouter);
+AppRouter.use("/users", authMiddleWare, UserRouter);
+AppRouter.use("/users", UserRouter);
+
+module.exports = { AppRouter };
